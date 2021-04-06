994c547df65d23aa11eaf4e07a58354b19512046
Roohi Singh <singhroohi184@gmail.com>
2021-04-06 18:03:48 -0400

    login and register ui

--- app/src/main/java/com/example/retailstudios/activities/MainActivity.kt
+++ app/src/main/java/com/example/retailstudios/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.retailstudios
+package com.example.retailstudios.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.retailstudios.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/retailstudios/activities/RegisterActivity.kt
@@ -0,0 +1,47 @@
+package com.example.retailstudios.activities
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.example.retailstudios.R
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        setupActionBar()
+
+        tv_login.setOnClickListener {
+            val intent = Intent(this@RegisterActivity,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(tv_toolbar_register_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24)
+        }
+
+        tv_toolbar_register_activity.setNavigationOnClickListener{onBackPressed()}
+    }
+}
