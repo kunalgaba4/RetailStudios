342ab573f9eab0e01275e41d0c12b0c921c9943a
Roohi Singh <singhroohi184@gmail.com>
2021-04-07 16:46:26 -0400

    Register user to firebase succesfully

--- app/src/main/java/com/example/retailstudios/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/retailstudios/activities/RegisterActivity.kt
@@ -8,6 +8,10 @@ import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.example.retailstudios.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : BaseActivity() {
@@ -35,9 +39,9 @@ class RegisterActivity : BaseActivity() {
             startActivity(intent)
         }
 
-        //validate details on button click
+        //validate details on button click and register user to firebase
         btn_register.setOnClickListener{
-            validateRegisterDetails()
+           registerUser()
         }
     }
 
@@ -58,38 +62,66 @@ class RegisterActivity : BaseActivity() {
     private fun validateRegisterDetails(): Boolean{
         return when {
             TextUtils.isEmpty(et_first_name.text.toString().trim { it <= ' ' }) -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), errorMessage = true)
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
                 false
             }
             TextUtils.isEmpty(et_last_name.text.toString().trim { it <= ' ' }) -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), errorMessage = true)
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
                 false
             }
             TextUtils.isEmpty(et_email.text.toString().trim { it <= ' ' }) -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), errorMessage = true)
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
                 false
             }
             TextUtils.isEmpty(et_password.text.toString().trim { it <= ' ' }) -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), errorMessage = true)
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
                 false
             }
             TextUtils.isEmpty(et_confirm_password.text.toString().trim { it <= ' ' }) -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), errorMessage = true)
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
                 false
             }
             et_password.text.toString().trim { it <= ' ' } != et_confirm_password.text.toString().trim { it <= ' ' } -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch),errorMessage = true)
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch),true)
                 false
             }
             !cb_terms_and_condition.isChecked -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition),errorMessage = true)
+                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition),true)
                 false
             }
             else ->
             {
-               showErrorSnackBar(resources.getString(R.string.registering_successful),errorMessage = false)
+               //showErrorSnackBar(resources.getString(R.string.registering_successful),false)
                 true
             }
+        }
+    }
+
+    //registering the user using firebase
+    private fun registerUser(){
+
+        //check with validate function if the entries are valid or not
+        if (validateRegisterDetails()){
+            val email:String = et_email.text.toString().trim{it <= ' '}
+            val password:String = et_password.text.toString().trim{it <= ' '}
+
+            //create an instance and create a register a user with email and password
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(
+                    OnCompleteListener <AuthResult>{ task ->
+                        //if the registration is successfully done
+                        if (task.isSuccessful){
+                            //firebase registered user
+                            val firebaseUser:FirebaseUser = task.result!!.user!!
+
+                            showErrorSnackBar("You are registered successfully. Your user id is ${firebaseUser.uid}",false)
+                        }
+                        else{
+                            //if the registering is not successful then show error message
+                            showErrorSnackBar(task.exception!!.message.toString(),true)
+                        }
+                    }
+                )
         }
     }
 }
