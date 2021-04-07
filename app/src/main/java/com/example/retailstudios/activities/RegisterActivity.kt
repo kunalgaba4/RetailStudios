101b3d16415a8933a6f65076bb5e9c9c56fb77d0
Roohi Singh <singhroohi184@gmail.com>
2021-04-07 15:45:36 -0400

    added snackbar to show error or success on RegisterActivity.kt for user entry (validation)

--- app/src/main/java/com/example/retailstudios/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/retailstudios/activities/RegisterActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.example.retailstudios.R
 import kotlinx.android.synthetic.main.activity_register.*
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -24,7 +25,8 @@ class RegisterActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
-        
+
+        //function calling
         setupActionBar()
 
         //go to login page
@@ -33,8 +35,13 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        //validate details on button click
+        btn_register.setOnClickListener{
+            validateRegisterDetails()
+        }
     }
 
+    //setup action bar to display arrow back
     private fun setupActionBar(){
         setSupportActionBar(tv_toolbar_register_activity)
 
@@ -45,5 +52,44 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         tv_toolbar_register_activity.setNavigationOnClickListener{onBackPressed()}
+    }
+
+    //function to validate the entries of user
+    private fun validateRegisterDetails(): Boolean{
+        return when {
+            TextUtils.isEmpty(et_first_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), errorMessage = true)
+                false
+            }
+            TextUtils.isEmpty(et_last_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), errorMessage = true)
+                false
+            }
+            TextUtils.isEmpty(et_email.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), errorMessage = true)
+                false
+            }
+            TextUtils.isEmpty(et_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), errorMessage = true)
+                false
+            }
+            TextUtils.isEmpty(et_confirm_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), errorMessage = true)
+                false
+            }
+            et_password.text.toString().trim { it <= ' ' } != et_confirm_password.text.toString().trim { it <= ' ' } -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch),errorMessage = true)
+                false
+            }
+            !cb_terms_and_condition.isChecked -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition),errorMessage = true)
+                false
+            }
+            else ->
+            {
+               showErrorSnackBar(resources.getString(R.string.registering_successful),errorMessage = false)
+                true
+            }
+        }
     }
 }
