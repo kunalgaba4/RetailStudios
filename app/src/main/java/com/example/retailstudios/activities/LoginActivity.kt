53a21bde3e95e901a603f170194d63e4a0cf5acf
Roohi Singh <singhroohi184@gmail.com>
2021-04-08 14:14:31 -0400

    added validation on login screen

--- app/src/main/java/com/example/retailstudios/activities/LoginActivity.kt
+++ app/src/main/java/com/example/retailstudios/activities/LoginActivity.kt
@@ -5,12 +5,15 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.text.TextUtils
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.example.retailstudios.R
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -21,16 +24,45 @@ class LoginActivity : AppCompatActivity() {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
         } else {
             window.setFlags(
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
+    }
 
-        //go to register page
-        tv_register.setOnClickListener {
-            val intent = Intent(this@LoginActivity,RegisterActivity::class.java)
-            startActivity(intent)
+    //In login screen the clickable components are Login Button, ForgetPassword and Register Text
+    override fun onClick(view: View?) {
+        if (view != null) {
+            when (view.id) {
+                R.id.tv_forget_password -> {
+
+                }
+                R.id.btn_login -> {
+                    validateLoginDetails()
+                }
+                R.id.tv_register -> {
+                    // Launch the register screen when the user clicks on the text
+                    val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                    startActivity(intent)
+                }
+            }
         }
+    }
 
+    private fun validateLoginDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(et_email.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+            TextUtils.isEmpty(et_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+            else -> {
+                showErrorSnackBar("Your details are valid", false)
+                true
+            }
+        }
     }
 }
